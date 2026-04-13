88f9237a8316d339a5c3106ab19d2a2b12496aaf
Magnus <magnustrier@gmail.com>
2026-04-13 07:58:29 +0200

    changed mountAppComponent to be more responsive on page change

--- src/components/mountAppContent.tsx
+++ src/components/mountAppContent.tsx
@@ -1,11 +1,8 @@
-import { lazy } from "react"
-import { AnimatePresence } from "motion/react"
 import Navbar from "./navbar/navbar"
 import { Routes, Route, Navigate, useLocation } from "react-router-dom"
-
-const WeightPage = lazy(() => import("./pages/weightPage/weightPage"))
-const WorkoutPage = lazy(() => import("./pages/workoutPage/workoutPage"))
-const SettingsPage = lazy(() => import("./pages/settingsPage/settingsPage"))
+import WeightPage from "./pages/weightPage/weightPage"
+import WorkoutPage from "./pages/workoutPage/workoutPage"
+import SettingsPage from "./pages/settingsPage/settingsPage"
 
 const MountAppContent = () => {
 	const location = useLocation()
@@ -13,16 +10,14 @@ const MountAppContent = () => {
 		<div
 			className="app-container"
 		>
-			<AnimatePresence mode="wait">
-				<Routes location={location} key={location.pathname}>
-					<Route path="/weight" element={<WeightPage />} />
-					<Route path="/workout" element={<WorkoutPage />} />
+			<Routes location={location} key={location.pathname}>
+				<Route path="/weight" element={<WeightPage />} />
+				<Route path="/workout" element={<WorkoutPage />} />
 
-					<Route path="/settings" element={<SettingsPage />} />
+				<Route path="/settings" element={<SettingsPage />} />
 
-					<Route path="/" element={<Navigate to="/weight" />} />
-				</Routes>
-			</AnimatePresence>
+				<Route path="/" element={<Navigate to="/weight" />} />
+			</Routes>
 			<Navbar />
 		</div>
 	)
